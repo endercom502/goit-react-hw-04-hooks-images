ace237e4c96acbbf95812a529d26efa8211e9bf6
Andrzej Markiewicz <endercom502@gmail.com>
2022-11-28 22:30:19 +0100

    hook_api

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -4,9 +4,9 @@ axios.defaults.baseURL = 'https://pixabay.com/api/';
 
 const API_KEY = "29528567-7e538c7cf33df2404e5c0ef32";
 
-export const getImages = async(searchValue, page, perPage) => {
+export const getImages = async (searchValue, page) => {
  
-    const response = await axios.get(`?key=${API_KEY}&q=${searchValue}&image_type=photo&orientation=horizontal&safeSearch=true&page=${page}&per_page=${perPage}`);
-    return response.data
-
-}
+    return axios.get(
+    `?key=${API_KEY}&q=${searchValue}&image_type=photo&orientation=horizontal&safeSearch=true&page=${page}&per_page=12`
+  );
+};
